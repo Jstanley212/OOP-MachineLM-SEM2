dadafe7669ca301be206462339a9d5a7c2c80e44
Joshua Stanley <joshuastanley@Joshuas-MBP.lan>
2025-04-29 13:26:42 +0100

    Level 3 functionality complete, added training data panel class so user can add more rows of data and added it to main frame using JTabbedPane

--- Code/MainFrame.java
+++ Code/MainFrame.java
@@ -15,13 +15,19 @@ public class MainFrame {
     //creating for GUI user inputs fields
     private InputPanel input_class;
 
+    //creating trainingDataPanel
+    private TrainingDataPanel training_panel;
 
 
     //constructor
     public MainFrame(){
 
+        //initialising panels
         main_screen = new JFrame("Primary Window");
         input_panel = new JPanel();
+
+        //initialising JTabbedPane to toggle between predict and adding new data
+        JTabbedPane two_Panes = new JTabbedPane();
 
         //initializing input panel and model
         NaiveBayes classifier = new NaiveBayes();
@@ -39,8 +45,31 @@ public class MainFrame {
         input_panel.add(input_class.getTrainButton());
         input_panel.add(input_class.getSubmit_entries());
 
+        // creating training panel
+        JPanel training_container = new JPanel();
+        training_panel = new TrainingDataPanel(classifier);
+
+        //adding training data panel dropdowns and buttons
+        training_panel = new TrainingDataPanel(classifier);
+        training_panel.add(training_panel.getAge_label());
+        training_panel.add(training_panel.getAgeGroup());
+        training_panel.add(training_panel.getVehicle_label());
+        training_panel.add(training_panel.getVehicleType());
+        training_panel.add(training_panel.getViolation_label());
+        training_panel.add(training_panel.getPriorViolation());
+        training_panel.add(training_panel.getMaintenance_label());
+        training_panel.add(training_panel.getMaintenanceRecord());
+        training_panel.add(training_panel.getHasviolation_label());
+        training_panel.add(training_panel.getHasViolation());
+        training_panel.add(training_panel.getAdd_button());
+        training_panel.add(training_panel.getUpdate_button());
+
         //adding input panel to frame
-        main_screen.add(input_panel);
+        two_Panes.addTab("Predict",input_panel);
+        //adding training_panel to frame
+        two_Panes.addTab("Add Data",training_panel);
+
+        main_screen.add(two_Panes);
 
         //setting size of frame and setting it to visible
         main_screen.setSize(600, 400);
--- Code/TrainingDataPanel.java
+++ Code/TrainingDataPanel.java
@@ -3,17 +3,210 @@ package Code;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class TrainingDataPanel extends BasePanel implements ActionListener{
+
+    //creating button to submit
+    private JButton add_button;
+    private JButton update_button;
+
+    //dropdown string list
+    private String[] age;
+    private String[] car_truck;
+    private String[] yes_no;
+    private String[] good_poor;
+
+    private JLabel age_label;
+    private JLabel vehicle_label;
+    private JLabel violation_label;
+    private JLabel maintenance_label;
+    private JLabel hasviolation_label;
 
     //creating button to add and update model
     private JButton addDataButton;
     private JButton updateModelbutton;
 
+    //creating boxes to add more rows to dataset
+    private JComboBox<String> ageGroup;
+    private JComboBox<String> vehicleType;
+    private JComboBox<String> priorViolation;
+    private JComboBox<String> maintenanceRecord;
+    private JComboBox<String> hasViolation;
 
+    //creating dataloader and model
+    private DataLoader dataLoader;
+    private NaiveBayes naiveBayesClassifier;
 
-    public TrainingDataPanel(){
+    //constructor
+    public TrainingDataPanel(NaiveBayes classifier){
+        //calling constructor
+        super();
+
+        this.naiveBayesClassifier = classifier;
+
+        this.dataLoader = new DataLoader();
+        initializeComponents();
+    }
+
+    @Override
+    public void initializeComponents() {
+        //dropdown options
+        age = new String[]{"Young", "Adult"};
+        car_truck = new String[]{"Car", "Truck"};
+        yes_no = new String[]{"Yes", "No"};
+        good_poor = new String[]{"Good", "Poor"};
+
+        //initialising JComboBox to enter user features into dataset and the JLabel,
+        ageGroup = new JComboBox<>(age);
+        age_label = new JLabel("Age Group");
+        vehicleType = new JComboBox<>(car_truck);
+        vehicle_label = new JLabel("Vehicle Type");
+        priorViolation = new JComboBox<>(yes_no);
+        violation_label = new JLabel("Prior Violation");
+        maintenanceRecord = new JComboBox<>(good_poor);
+        maintenance_label = new JLabel("Maintenance Record");
+        hasViolation = new JComboBox<>(yes_no);
+        hasviolation_label = new JLabel("Violation");
+
+        //button to submit the text fields and an actionListener attached
+        add_button = new JButton("Add Data");
+        add_button.addActionListener(this);
+
+        //initializing training button
+        update_button = new JButton("Update");
+        update_button.addActionListener(this);
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == add_button) {
+            addTrainingData();
+        } else if (e.getSource() == update_button) {
+            updateModel();
+        }
+    }
+
+    private void addTrainingData() {
+
+        //gets the values entered in the combo boxes by the user and stores them in String newData
+        String newData = String.format("%s,%s,%s,%s,%s",
+                ageGroup.getSelectedItem(),
+                vehicleType.getSelectedItem(),
+                priorViolation.getSelectedItem(),
+                maintenanceRecord.getSelectedItem(),
+                hasViolation.getSelectedItem()
+        );
+
+        //file writer to data to end of file, append mode is true
+        try (FileWriter fw = new FileWriter("vehicle_has_violation.csv", true);
+
+             //buffered writer and print writer for efficient writing to file
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+
+            //file being written too
+            File file = new File("vehicle_has_violation.csv");
+
+            //checks if file ends with a newline, if file doesn't have one adds it before writing
+            if (file.length() > 0) {
+
+                //random access file since we only want to know whats at the end of the file
+                RandomAccessFile raf = new RandomAccessFile(file, "r");
+
+                //moves pointer to last point of file, since we are checking the end of the file
+                raf.seek(file.length() - 1);
+
+                //reading into last byte
+                byte endFile = raf.readByte();
+                //close file
+                raf.close();
+
+                //checking if end of file is \n
+                if (endFile != '\n') {
+                    //adding newline if needed
+                    out.println();
+                }
+            }
+
+            //write the new data using print writer
+            out.println(newData);
+
+            JOptionPane.showMessageDialog(this,
+                    "Training data added successfully!",
+                    "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this,
+                    "Error adding training data: " + ex.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void updateModel() {
+        try {
+            naiveBayesClassifier.train(dataLoader.loadingData());
+            JOptionPane.showMessageDialog(this,
+                    "Model updated successfully with new training data!",
+                    "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this,
+                    "Error updating model: " + ex.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    // Getters for components
+    public JButton getAdd_button() {
+        return add_button;
+    }
+
+    public JButton getUpdate_button() {
+        return update_button;
+    }
+
+    public JLabel getAge_label() {
+        return age_label;
+    }
+
+    public JComboBox<String> getAgeGroup() {
+        return ageGroup;
+    }
+
+    public JLabel getVehicle_label() {
+        return vehicle_label;
+    }
+
+    public JComboBox<String> getVehicleType() {
+        return vehicleType;
+    }
+
+    public JLabel getViolation_label() {
+        return violation_label;
+    }
+
+    public JComboBox<String> getPriorViolation() {
+        return priorViolation;
+    }
+
+    public JLabel getMaintenance_label() {
+        return maintenance_label;
+    }
+
+    public JComboBox<String> getMaintenanceRecord() {
+        return maintenanceRecord;
+    }
+
+    public JLabel getHasviolation_label() {
+        return hasviolation_label;
+    }
+
+    public JComboBox<String> getHasViolation() {
+        return hasViolation;
+    }
 }
